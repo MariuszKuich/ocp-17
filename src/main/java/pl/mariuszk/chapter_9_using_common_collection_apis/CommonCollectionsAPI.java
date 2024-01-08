fbb2dacb185ba08fd4168aabbc6ef39a6f8eb4ca
MariuszKuich <mariuszkuich@outlook.com>
2024-01-08 17:46:37 +0100

    08.01 - Lambdas and Functional Interfaces

--- src/main/java/pl/mariuszk/chapter_9_using_common_collection_apis/CommonCollectionsAPI.java
+++ src/main/java/pl/mariuszk/chapter_9_using_common_collection_apis/CommonCollectionsAPI.java
@@ -1,8 +1,6 @@
 package pl.mariuszk.chapter_9_using_common_collection_apis;
 
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.HashSet;
+import java.util.*;
 
 // These common methods are available in classes that implement the `Collection` interface.
 public class CommonCollectionsAPI {
@@ -23,16 +21,41 @@ public class CommonCollectionsAPI {
         System.out.println(list);
         System.out.println("----");
 
-        // isEmpty() and size() are straightforward
+        // isEmpty() and size() are straightforward.
         System.out.println(list.isEmpty());
         System.out.println(list.size());
         System.out.println("----");
 
-        // clear() discards all elements of the collection
+        // clear() discards all elements of the collection.
         list.clear();
         System.out.println(list.isEmpty());
         System.out.println("----");
 
+        // contains() calls equals() on objects to look for matches.
+        list.add("David");
+        System.out.println(list.contains("David"));
+        System.out.println("----");
 
+        // removeIf() accepts a Predicate and removes all elements that meet the condition.
+        list.add("Dawid");
+        list.removeIf(s -> s.contains("v"));
+        System.out.println(list);
+        System.out.println("----");
+
+        // forEach() accepts a Consumer.
+        list.add("Andrew");
+        list.forEach(System.out::println);
+        System.out.println("----");
+
+        // equals() method accepts an Object and allows to compare the type and contents of two collections.
+        // Implementations vary between collections, e.g. lists care for order, while sets do not.
+        List<Integer> l1 = List.of(1, 2);
+        List<Integer> l2 = List.of(2, 1);
+        Set<Integer> s1 = Set.of(1, 2);
+        Set<Integer> s2 = Set.of(2, 1);
+        System.out.println(l1.equals(l2));
+        System.out.println(s1.equals(s2));
+        // Contents are the same, but the types are not, so these collections are not equal.
+        System.out.println(l1.equals(s1));
     }
 }
--- src/main/resources/chapter_9_using_common_collection_apis/notes.md
+++ src/main/resources/chapter_9_using_common_collection_apis/notes.md
@@ -3,3 +3,10 @@ Java Collections Framework - a group of classes for storing collections from the
 `List`, `Queue` and `Set` interfaces extend the `Collection` interface. `Map` interface doesn't extend that interface.
 
 Common methods from the Collections API: see `CommonCollectionsAPI` file.
+
+`List` interface:\
+List can be used when an ordered collection that allows duplicates is needed.\
+Items can be inserted and retrieved at specific positions based on the `int` index.\
+Many List implementations can change size after they are declared.\
+All List implementations allow duplicates and are ordered collections, but beyond that, the offer different functionalities.
+
